bed29a618858f413c3bc0b853efce8df41a0485a
JonaFer18 <71918026+JonaFer18@users.noreply.github.com>
2021-05-26 17:12:15 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,114 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const url = require('url');
+const path = require('path');
+if (process.env.NODE_ENV !== 'production')
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+    })
+
+let mainWindow;
+let newProductWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true,
+            contextIsolation: false,
+        }
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/index.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+    const mainMenu = Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(mainMenu);
+    mainWindow.on('closed', () => {
+        app.quit();
+    });
+});
+
+ipcMain.on('product:new', (e, newProduct) => {
+    mainWindow.webContents.send('product:new', newProduct);
+    console.log(newProduct);
+    newProductWindow.close();
+});
+
+const templateMenu = [{
+        label: 'File',
+        submenu: [{
+                label: 'New Product',
+                accelerator: 'Ctrl+N',
+                click() {
+                    createNewProductWindow();
+                }
+            },
+            {
+                label: 'Remove All Product',
+                click() {
+                    mainWindow.webContents.send('products:remove-all');
+                }
+            },
+            {
+                label: 'Exit',
+                accelerator: process.platform == 'darwin' ? 'command+Q' : 'Ctrl+Q',
+
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+
+    },
+
+
+];
+
+function createNewProductWindow() {
+    newProductWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            nodeIntegrationInWorker: true,
+            contextIsolation: false,
+        },
+        width: 400,
+        height: 330,
+        title: 'Add A New Product'
+    });
+    newProductWindow.setMenu(null);
+    newProductWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/new-product.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+    newProductWindow.on('closed', () => {
+        newProductWindow = null;
+    });
+}
+
+if (process.platform === 'darwin') {
+    templateMenu.unshift({
+        label: app.getName(),
+    });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    templateMenu.push({
+        label: 'DevTools',
+        submenu: [{
+                label: 'Show/Hide Dev Tools',
+                accelerator: 'Ctrl+D',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+
+}
